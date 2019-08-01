70c2cc8fdf225f452afa044ce17cb169283b884c
Luchev <luchevz@gmail.com>
2019-08-01 13:03:05 +0300

    Add fibonacci calculated with the formula in O(1)

---
+++ fibonacci/fibonacci-formula.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * Calculate the Nth fibonacci term using the formula
+ *
+ * Time complexity: O(1)
+ * Space complexity: O(1)
+ *
+ * Problems: The low precision of type double makes the program work
+ * only for the first 60ish terms.
+ *
+ * Compile: gcc -lm fibonacci-formula.c
+ */
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("Provide exactly one number as arguement\n");
+		return 1;
+	}
+
+	int ithTerm = atoi(argv[1]);
+	long double sqrt5 = sqrt(5);
+	unsigned long long fibonacci = (pow((1 + sqrt5)/2, ithTerm) - pow((1 - sqrt5)/2, ithTerm)) / sqrt5;
+	printf("%lu\n", fibonacci);
+	return 0;
+}
